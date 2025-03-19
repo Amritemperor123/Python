89e2002cd38eac543574f4488264d351ccd25e36
Amrit Lal Paswan <amritpaswan5952@gmail.com>
2025-03-19 14:23:33 +0530

    Updated Perceptron Learning Algorithm for OR

--- 35. Perceptron Learning Algorithm for OR.py
+++ 35. Perceptron Learning Algorithm for OR.py
@@ -1,5 +1,11 @@
+# up until now, we were assigning weights and bias to the perceptrons mannunally, which is not practical to build a neural network
+# this time, we will assign some initial values to the weights and bias, and then keep changing them to get the preffered output
+
 import numpy as np
 
+# we create an input dataset X, which contains the sets of input values 
+# y is the dataset which has the output values for each input set
+# our goal is to get the exact values of weights and bias which will give us the desired outputs when an input set is given
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 1, 1, 1]) 
 
@@ -8,9 +14,13 @@ y = np.where(y == 1, 1, -1)
 weights = np.zeros(2)
 bias = 0
 
+# signum function return 1 if a number if positive, else -1.
 def signum(value):
     return 1 if value >= 0 else -1
 
+# the trainning algorithm is where we make changes to the weights and bias
+# convergence is a condition where the predicted output and the desired output becomes same
+# by changing the weights and bias to reach convergence, we get the exact value we need
 def train_perceptron():
     global weights, bias
     converged = False
